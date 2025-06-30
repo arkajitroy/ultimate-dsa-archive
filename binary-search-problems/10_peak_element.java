34b137ee8e926f92df26cb99c03a95843720eab4
Arkajit Roy <arkajitroy11.official@gmail.com>
2025-06-30 15:21:22 +0530

    added peak element bs

--- binary-search-problems/10_peak_element.java
+++ binary-search-problems/10_peak_element.java
@@ -3,13 +3,18 @@
 // Approach: Binary Search
 class Solution {
     public int findPeakElement(int[] nums) {
-        int left = 0, right = nums.length-1;
+        int n = nums.length;
+
+        if (n == 1) return 0;
+        
+        int left = 0, right = n - 1;
         while (left < right) {
             int mid = left + (right - left) / 2;
-            // peak is in the left half
-            if(nums[mid] > nums[mid + 1]) right = mid;
-            // peak is in the right half
-            else right = mid + 1;
+            if (nums[mid] > nums[mid + 1]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
         }
         return left;
     }
