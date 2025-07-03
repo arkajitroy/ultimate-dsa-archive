31397b1b2d3fa452a6211dbfa5782e352334d07b
Arkajit Roy <arkajitroy11.official@gmail.com>
2025-07-03 10:24:10 +0530

    added: a follow up question for the MST

--- graph-problems/15_minimum_spanning_tree.java
+++ graph-problems/15_minimum_spanning_tree.java
@@ -22,16 +22,59 @@ class Solution {
             int weight = current[1];
 
             if(visited[node]) continue; // if it is visited then we will go to next node
+            
+            visited[node] = true;
             MSTSum += weight;
 
-            for(int[] neighbor: adj.get(node)){
+            for (int[] neighbor : adj.get(node)) {
                 int adjNode = neighbor[0];
-                int edgWeight = neighbor[1];
+                int edgeWeight = neighbor[1];
 
-                if(!visited[adjNode]) pq.add(new int[]{ adjNode, edgWeight });
+                if (!visited[adjNode]) pq.add(new int[]{adjNode, edgeWeight});
             }
         }
         return MSTSum;
+    }
+
+    // Follow up question : A list of edges that form the MST.
+    static List<List<Integer>> mstWithEdges(int V, int E, List<List<int[]>> adj) {
+        boolean[] visited = new boolean[V];
+        int[] parent = new int[V];        // To store parent of each node in MST
+        int[] weights = new int[V];           // To store min edge weights
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> a[1] - b[1]); // (node, weight)
+        
+        Arrays.fill(weights, Integer.MAX_VALUE);
+        Arrays.fill(parent, -1);
+
+        weights[0] = 0;
+        pq.add(new int[]{0, 0});
+
+        while (!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int u = curr[0];
+
+            if (visited[u]) continue;
+            visited[u] = true;
+
+            for (int[] neighbor : adj.get(u)) {
+                int v = neighbor[0];
+                int wt = neighbor[1];
+
+                if (!visited[v] && wt < weights[v]) {
+                    weights[v] = wt;
+                    parent[v] = u;
+                    pq.add(new int[]{v, wt});
+                }
+            }
+        }
+
+        // Build the result list of edges
+        List<List<Integer>> mstEdges = new ArrayList<>();
+        for (int i = 1; i < V; i++) {
+            mstEdges.add(Arrays.asList(parent[i], i, weights[i]));
+        }
+
+        return mstEdges;
     }
 
     public static void main(String[] args) {
@@ -62,5 +105,11 @@ class Solution {
         adj.get(4).add(new int[]{2, 7});
 
         System.out.println("Total weight of MST: " + spanningTree(V, E, adj));
+
+        List<List<Integer>> mst = mstWithEdges(V, E, adj);
+        System.out.println("Edges in MST:");
+        for (List<Integer> edge : mst) {
+            System.out.println("From : " + edge.get(0) + " -> " + edge.get(1) + " : (w) " + edge.get(2));
+        }
     }
 }
